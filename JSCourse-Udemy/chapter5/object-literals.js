d0b0500132a617aa92666354ce4f5a62e0e146f9
acorreiaa <amandaluana777@gmail.com>
2020-12-18 07:42:35 -0300

    Feat JS Chapter5 Initial Math Object

--- JSCourse-Udemy/chapter5/object-literals.js
+++ JSCourse-Udemy/chapter5/object-literals.js
@@ -5,20 +5,34 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & chese rules', '10 things to make with marmite'], 
+//    blogs: ['why mac & chese rules', '10 things to make with marmite'],
+// string array (acima) e object string(abaixo) 
+    blogs: [
+        {title: 'why mac & chese rules', likes: 30},
+        {title: '10 things to make with marmite', likes: 50}
+    ],
     login: function(){
         console.log('the user logged in');
     },
     logout: function(){
         console.log('the user logged out');
     },
-    logBlogs: function(){
-        
+    logBlogs(){
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+//            console.log(blog);
+            console.log(blog.title, blog.likes);
+        });
     }
 };
+
+user.logBlogs();
+console.log(this);
+
 //Invocando um metodo => login, nova propriedade do objeto USER
-user.login();
-user.logout();
+//user.login();
+//user.logout();
 
 //console.log(user);
 //console.log(user.name);
---
+++ JSCourse-Udemy/chapter5/object-math.js
@@ -0,0 +1,17 @@
+// Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+console.log(Math.round(area)); // arredonda o valor pela média
+console.log(Math.floor(area)); // arredonda o valor para baixo
+console.log(Math.ceil(area));  // arredonda o valor para cima
+console.log(Math.trunc(area)); // arredonda o valor eliminando o valor decimal 
+
+
+// Random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
