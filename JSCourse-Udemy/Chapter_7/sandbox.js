9a0c1373f651271f8662ab6995d410d9c213ba17
acorreiaa <amandaluana777@gmail.com>
2021-01-15 07:04:11 -0300

    Feat JS Regular Expression

--- JSCourse-Udemy/Chapter_7/sandbox.js
+++ JSCourse-Udemy/Chapter_7/sandbox.js
@@ -4,5 +4,24 @@ const form = document.querySelector('.signup-form');
 form.addEventListener('submit', e => {
     e.preventDefault();
 //    console.log(username.value);
-    console.log(form.username.value);  //username inibido pq já está dentro do formulário
+    console.log(form.username.value);  //username comentado pq já está dentro do formulário
 });
+
+//REGULAR EXPRESSION - Valida caracteres de um campo
+const username = '@shaunp5421'
+//const pattern = /[a-z]{6,}/;  // True: Considera APENAS se há caracteres na condição
+const pattern = /^[a-z]{6,}$/;  // False: Considera todos caracteres da string (^ e $)
+
+//let result = pattern.test(username);
+//console.log(result)
+
+//if(result){
+//    console.log('regular express passed :)');    
+//}   else {
+//    console.log('regular express failed :(');
+//}
+
+let result = username.search(pattern);
+console.log(result);   //@shaunp5421 com ^ e $ = negativo (-1) e positivo (1) sem ^ e $
+
+
