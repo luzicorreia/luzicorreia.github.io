b5c022964d6ca956b22aa14112fee706b796724b
acorreiaa <amandaluana777@gmail.com>
2021-01-18 06:41:44 -0300

    Feat JS Validation Regular Expression

--- JSCourse-Udemy/Chapter_7/sandbox.js
+++ JSCourse-Udemy/Chapter_7/sandbox.js
@@ -1,16 +1,17 @@
 const form = document.querySelector('.signup-form');
 //const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 //    console.log(username.value);
-    console.log(form.username.value);  //username comentado pq já está dentro do formulário
-});
+//    console.log(form.username.value);  //username comentado pq já está dentro do formulário
+
 
 //REGULAR EXPRESSION - Valida caracteres de um campo
-const username = '@shaunp5421'
+//const username = '@shaunp5421'
 //const pattern = /[a-z]{6,}/;  // True: Considera APENAS se há caracteres na condição
-const pattern = /^[a-z]{6,}$/;  // False: Considera todos caracteres da string (^ e $)
+//const pattern = /^[a-z]{6,}$/;  // False: Considera todos caracteres da string (^ e $)
 
 //let result = pattern.test(username);
 //console.log(result)
@@ -21,7 +22,19 @@ const pattern = /^[a-z]{6,}$/;  // False: Considera todos caracteres da string (
 //    console.log('regular express failed :(');
 //}
 
-let result = username.search(pattern);
-console.log(result);   //@shaunp5421 com ^ e $ = negativo (-1) e positivo (1) sem ^ e $
+//let result = username.search(pattern);
+//console.log(result);   //@shaunp5421 com ^ e $ = negativo (-1) e positivo (1) sem ^ e $
 
+//VALIDATION
+const username = form.username.value;
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
+if(usernamePattern.test(username)){
+    //feedback good info
+    feedback.textContent = 'that username is valid!';
+}else {
+    //feedback help info
+    feedback.textContent = 'username must contain letters only & be between 6 & 12 characters long'; 
+}
+
+});
