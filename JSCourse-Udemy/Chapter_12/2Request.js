de5accf22e3377e66c226ea14da86b0a2d394b26
acorreiaa <amandaluana777@gmail.com>
2021-03-08 06:59:28 -0300

    Feat JS Request ResponseText ReadyState Chapter12

---
+++ JSCourse-Udemy/Chapter_12/2Request.js
@@ -0,0 +1,24 @@
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    //console.log(request, request.readyState)
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("could not fetch the data", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
+  request.send();
+};
+
+getTodos((err, data) => {
+  console.log("callback fired");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
