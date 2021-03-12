94fd02ad0b43bdc8b3cb53f8083594a439fb58e9
acorreiaa <amandaluana777@gmail.com>
2021-03-12 06:22:54 -0300

    Feat JS CallbackHell Chapter12

--- JSCourse-Udemy/Chapter_12/2Request.js
+++ JSCourse-Udemy/Chapter_12/2Request.js
@@ -1,10 +1,10 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
     //console.log(request, request.readyState)
     if (request.readyState === 4 && request.status === 200) {
-      const data =JSON.parse(request.responseText);  //transforma objeto JSON em objeto JS
+      const data = JSON.parse(request.responseText); //transforma objeto JSON em objeto JS
       callback(undefined, data);
     } else if (request.readyState === 4) {
       callback("could not fetch the data", undefined);
@@ -13,23 +13,16 @@ const getTodos = (callback) => {
 
   //request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
   //request.open("GET", 'todos.json');
-  request.open("GET", 'todos/luigi.json');
-
+  request.open("GET", resource);
   request.send();
 };
 
-console.log(1);
-console.log(2);
-
-getTodos((err, data) => {
-  console.log("callback fired");
-  if (err) {
-    console.log(err);
-  } else {
+getTodos("todos/luigi.json", (err, data) => {
+  console.log(data);
+  getTodos("todos/mario.json", (err, data) => {
     console.log(data);
-  }
+    getTodos("todos/shaun.json", (err, data) => {
+      console.log(data);
+    });
+  });
 });
-
-console.log(3);
-console.log(4);
-
