dc0895c3f5d48c243b02fa36bb474ee73c82cfbd
acorreiaa <amandaluana777@gmail.com>
2021-03-18 07:04:57 -0300

    Feat JS AsyncWait Fetch Chapter12

---
+++ JSCourse-Udemy/Chapter_12/fetchAPI.js
@@ -0,0 +1,26 @@
+//ASYNC & AWAIT
+const getTodos = async () => {
+  const response = await fetch("todos/luigi.json");
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data");
+  }
+  const data = await response.json();
+  return data;
+};
+
+getTodos()
+  .then((data) => console.log("resolved:", data))
+  .catch((err) => console.log("rejected:", err.message));
+
+// FETCH API
+//fetch("todos/luigis.json")
+//  .then((response) => {
+//    console.log("resolved", response);
+//    return response.json();
+//  })
+//  .then((data) => {
+//    console.log(data);
+//  })
+//  .catch((err) => {
+//    console.log("rejected", err);
+//  });
