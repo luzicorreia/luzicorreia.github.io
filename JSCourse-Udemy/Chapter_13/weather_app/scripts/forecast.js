132455cbe1a171d5838a75871e803bc816aca87c
acorreiaa <amandaluana777@gmail.com>
2021-03-25 06:48:40 -0300

    Feat JS UpdateLocation FormAppCity Chapter 13

--- JSCourse-Udemy/Chapter_13/weather_app/scripts/forecast.js
+++ JSCourse-Udemy/Chapter_13/weather_app/scripts/forecast.js
@@ -1,4 +1,16 @@
 const key = 'cpwNY9vlEBE4sBJJInUsqdDQ6jRYpzx8';
+
+//GET WEATHER INFORMATION
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    response = await fetch(base + query);
+    const data = await response.json();
+    return data[0]
+}
+
+//GET CITY INFORMATION
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -9,7 +21,3 @@ const getCity = async (city) => {
 
     return data[0]
 };
-
-getCity('manchester')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
