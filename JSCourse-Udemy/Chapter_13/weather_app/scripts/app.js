867a7aa7fc326c421d8c8e258076e8a4dbba4c04
acorreiaa <amandaluana777@gmail.com>
2021-03-29 07:00:10 -0300

    Feat JS ImageDayNightWeather Chapter 13

--- JSCourse-Udemy/Chapter_13/weather_app/scripts/app.js
+++ JSCourse-Udemy/Chapter_13/weather_app/scripts/app.js
@@ -1,4 +1,40 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card')
+const details = document.querySelector('.details') 
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    //desctructure properties  **** the same linhas 6 e 7 - substui as linhas 6 e 7 pela linha 10) 
+    const { cityDets, weather } = data 
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    //UPDATE THE NIGHT/DAY & ICON IMAGES
+    let timeSrc = null;
+    if(weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
+
+    //REMOVE THE d-none CLASS IF PRESENT
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+};
 
 const updateCity = async (city) => {
     const cityDets = await getCity(city);
@@ -17,6 +53,6 @@ cityForm.addEventListener('submit', e => {
 
     //update the ui with new city
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
