e438b38d1b997c7398900f626433b8f9c6b7277a
acorreiaa <amandaluana777@gmail.com>
2021-05-04 06:42:21 -0300

    Feat JS AddFormDataBase Chapter16

--- JSCourse-Udemy/Chapter_16/sandbox.js
+++ JSCourse-Udemy/Chapter_16/sandbox.js
@@ -1,4 +1,5 @@
 const list = document.querySelector("ul");
+const form = document.querySelector("form");
 
 const addRecipe = (recipe) => {
   let time = recipe.created_at.toDate();
@@ -22,3 +23,19 @@ db.collection("recipes")
   .catch((err) => {
     console.log(err);
   });
+
+  //ADD DOCUMENTS
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const now = new Date();
+    const recipe = {
+      title: form.recipe.value,
+      created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    db.collection('recipes').add(recipe).then(() => {
+      console.log('recipe added');
+    }).catch(err => {
+      console.log(err);
+    });
+  })
