a2bec603172a9e2a4b8741f28eeed6c2765d335d
acorreiaa <amandaluana777@gmail.com>
2021-05-05 07:02:50 -0300

    Feat JS DeleteItemDataBase Chapter16

--- JSCourse-Udemy/Chapter_16/sandbox.js
+++ JSCourse-Udemy/Chapter_16/sandbox.js
@@ -1,15 +1,16 @@
 const list = document.querySelector("ul");
 const form = document.querySelector("form");
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
   let time = recipe.created_at.toDate();
   let html = `
-    <li>
+    <li data-id="${id}">
     <div>${recipe.title}</div>
     <div>${time}</div>
+    <button class="btn btn-danger btn-sm-my-2">delete</button>  
     </li>
     `;
-  list.innerHTML +=html;
+  list.innerHTML += html;
 };
 
 db.collection("recipes")
@@ -17,25 +18,43 @@ db.collection("recipes")
   .then((snapshot) => {
     //WHEN WE HAVE THE RESPONSE DATA
     snapshot.docs.forEach((doc) => {
-      addRecipe(doc.data());
+      addRecipe(doc.data(), doc.id);
     });
   })
   .catch((err) => {
     console.log(err);
   });
 
-  //ADD DOCUMENTS
-  form.addEventListener('submit', e => {
-    e.preventDefault();
+//ADD DOCUMENTS
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const now = new Date();
-    const recipe = {
-      title: form.recipe.value,
-      created_at: firebase.firestore.Timestamp.fromDate(now)
-    };
-    db.collection('recipes').add(recipe).then(() => {
-      console.log('recipe added');
-    }).catch(err => {
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch((err) => {
       console.log(err);
     });
-  })
+});
+
+//DELETING DATA
+list.addEventListener("click", (e) => {
+  //console.log(e);
+  if (e.target.tagName === "BUTTON") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    //console.log(id);
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("recipe delete");
+      });
+  }
+});
